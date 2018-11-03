98a3a3f9efc43be713825d640dfe6ecc44175fca
Sameer99 <sameer.saptarshi>
2018-11-03 00:55:45 -0700

    first commit

---
+++ SpotifyQueue/app/src/main/java/com/example/sameer/spotifyqueue/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.sameer.spotifyqueue;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import static com.spotify.sdk.android.authentication.LoginActivity.REQUEST_CODE;
+//import com.spotify.sdk.android.authentication;
+
+public class MainActivity extends AppCompatActivity {
+    private String CLIENT_ID = "ea271345845f4705975a6811e189220f";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // We will start writing our code here.
+        int REQUEST_CODE = 1337;
+        String REDIRECT_URI = "yourcustomprotocol://callback";
+
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    String token = response.getAccessToken();
+                    System.out.println("ACCESS TOKEN: ");
+                    System.out.print(token);
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
+    }
+
+}
